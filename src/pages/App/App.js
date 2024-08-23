8dad201bc28e7f1e68eab8a4fc37eaf2f559e0e5
lcohen730 <lcohen730@gmail.com>
2024-08-23 01:36:22 -0400

    Add Nav Bar and Selection Page to App Page, with useState, useEffect, and useLocation

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
+import SelectionPage from '../SelectionPage/SelectionPage';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function App() {  
+  const [selection, setSelection] = useState(null);
+
+  const location = useLocation();
+
+  useEffect(function () {
+    setSelection('home');
+  }, [location.pathname === '/']);
+  
   return (
     <main>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" 
+          element={<HomePage 
+            selection={selection} 
+            setSelection={setSelection} 
+          />} 
+        />
+        <Route path="/selection" 
+          element={<SelectionPage 
+            selection={selection} 
+            setSelection={setSelection}
+          />} 
+        />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </main>
