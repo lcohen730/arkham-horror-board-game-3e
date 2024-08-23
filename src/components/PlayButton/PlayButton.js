0caa17508e98366795676cb6923d8282b9ac1a8d
lcohen730 <lcohen730@gmail.com>
2024-08-23 01:26:09 -0400

    Add Handle Click Function to Play Button that Navigates to Selection Page

--- src/components/PlayButton/PlayButton.js
+++ src/components/PlayButton/PlayButton.js
@@ -1,7 +1,14 @@
 import styles from './PlayButton.module.scss';
+import { useNavigate } from 'react-router-dom';
 
-export default function PlayButton() {
+export default function PlayButton({ setSelection }) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/selection');
+        setSelection('select scenario button');
+    };
+    
     return (
-        <h1 className={styles.playButton}>PLAY NOW</h1>
+        <h1 className={styles.playButton} onClick={handleClick}>PLAY NOW</h1>
     );
 }
