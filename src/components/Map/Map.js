fd3ec21b8b2cf854a91575061715e320913c0a2c
lcohen730 <lcohen730@gmail.com>
2024-08-29 17:11:26 -0400

    Add Map Component for Game Map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,10 @@
+export default function Map() {
+    return (
+        <div>
+            <img
+                src="/img/sample-map.png"
+                alt="Map"
+            />
+        </div>
+    );
+}
