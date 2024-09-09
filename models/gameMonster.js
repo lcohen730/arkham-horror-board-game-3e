1851173bc4afbf743c60904dd245500155d5082e
lcohen730 <lcohen730@gmail.com>
2024-09-08 23:23:37 -0400

    Add Exhausted Property to Game Monster Schema and Add Common, Update Health, and Toggle Exhausted Status Methods

--- models/gameMonster.js
+++ models/gameMonster.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-/* const monster = require('./monster');
-const playerInvestigator = require('./playerInvestigator');
-const location = require('./location');
-const street = require('./street'); */
+const commonMethods = require('./commonMethods');
 
 const gameMonsterSchema = new Schema({
     health: { type: Number, required: true },
     location: { type: Schema.Types.ObjectId, ref: 'Location' },
     street: { type: Schema.Types.ObjectId, ref: 'Street' },
     engagedInvestigator: { type: Schema.Types.ObjectId, ref: 'PlayerInvestigator' },
+    exhausted: { type: Boolean, required: true },
     monster: { type: Schema.Types.ObjectId, ref: 'Monster' }
 });
+
+gameMonsterSchema.methods.updateHealth = function (newHealth) {
+    this.health = newHealth;
+    return this.save();
+}
+
+// this method can be used for location or street
+gameMonsterSchema.methods.updateSchemaObject = commonMethods.updateSchemaObject;
+
+gameMonsterSchema.methods.toggleExhausted = function () {
+    this.exhausted = !this.exhausted;
+    return this.save();
+}
 
 module.exports = mongoose.model('GameMonster', gameMonsterSchema);
