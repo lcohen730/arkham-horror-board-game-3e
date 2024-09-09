51c9a1c2b0233a35de780b2211bce543e29fb4c0
lcohen730 <lcohen730@gmail.com>
2024-09-08 23:33:30 -0400

    Update Map Property to Refer to New GameMap Model, Add Anomaly Boolean Property, Add Common Methods, and Add Toggle Anomaly Status Method to Neighborhood Model

--- models/neighborhood.js
+++ models/neighborhood.js
@@ -1,18 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-/* const map = require('./map');
-const playerInvestigator = require('./playerInvestigator');
-const gameMonster = require('./gameMonster');
-const location = require('./location'); */
+const commonMethods = require('./commonMethods');
 
 const neighborhoodSchema = new Schema({
-    map: { type: Schema.Types.ObjectId, ref: 'Map' },
+    map: { type: Schema.Types.ObjectId, ref: 'GameMap' },
     name: { type: String, required: true },
     clues: { type: Number, required: true },
     investigators: [{ type: Schema.Types.ObjectId, ref: 'PlayerInvestigator' }],
     monsters: [{ type: Schema.Types.ObjectId, ref: 'GameMonster' }],
     doom: { type: Number, required: true },
-    locations: [{ type: Schema.Types.ObjectId, ref: 'Location' }]
+    locations: [{ type: Schema.Types.ObjectId, ref: 'Location' }],
+    anomaly: { type: Boolean, required: true }
 });
+
+neighborhoodSchema.methods.updateAmt = commonMethods.updateAmt;
+neighborhoodSchema.methods.addToArray = commonMethods.addToArray;
+neighborhoodSchema.methods.removeFromArray = commonMethods.removeFromArray;
+
+neighborhoodSchema.methods.toggleAnomaly = function () {
+    this.anomaly = !this.anomaly;
+    return this.save();
+}
 
 module.exports = mongoose.model('Neighborhood', neighborhoodSchema);
