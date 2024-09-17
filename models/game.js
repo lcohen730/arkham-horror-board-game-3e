47e1e0946e1a5bd50a0884f9a76f4f92efd26d86
lcohen730 <lcohen730@gmail.com>
2024-09-16 22:48:59 -0400

    Update Game Model to Use Schema.Types.ObjectId for Objects Instead of Variables

--- models/game.js
+++ models/game.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const playerInvestigator = require('./playerInvestigator');
-const gameScenario = require('./gameScenario');
 const commonMethods = require('./commonMethods');
 
 const gameSchema = new Schema({
-    investigators: [playerInvestigator],
-    scenario: gameScenario,
+    investigators: [{ type: Schema.Types.ObjectId, ref: 'PlayerInvestigator' }],
+    scenario: { type: Schema.Types.ObjectId, ref: 'GameScenario' },
     archive: { type: Array, required: true },
     headlines: { type: Array, required: true }
 });
