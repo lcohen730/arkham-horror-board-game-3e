ff1449313cc64d9b7772f1e16ab00cd0ae982eda
lcohen730 <lcohen730@gmail.com>
2024-09-21 00:16:26 -0400

    Fix Bug in Investigators Controller

--- controllers/api/investigators.js
+++ controllers/api/investigators.js
@@ -8,7 +8,10 @@ module.exports = {
 async function index(req, res) {
     try {
         const investigators = await Investigator.find({});
-        res.status(200).json(investigators);
+        if (!investigators) {
+            return res.status(404).json({ msg: 'No investigators found' });
+        }
+        res.json(investigators);
     } catch (e) {
         res.status(400).json({ msg: e.message });
     }
