0c38c594f0f6379db024d77f1fa57cece7ebf025
lcohen730 <lcohen730@gmail.com>
2025-03-04 00:18:13 -0500

    Updates to monster model: update movement property to not be required, since not all monsters move; add spawnLocation and spawnStreet properties

--- models/monster.js
+++ models/monster.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const monsterSchema = new Schema({
-    name: { type: String, required: true },
-    type: { type: String, required: true },
-    movement: { type: Number, required: true },
-    remnant: { type: Boolean, default: false, required: true },
-    health: { type: Number, required: true },
-    attackMod: { type: Number, required: true },
-    evadeMod: { type: Number, required: true },
-    healthDmg: { type: Number, required: true },
-    sanityDmg: { type: Number, required: true }
+	name: { type: String, required: true },
+	type: { type: String, required: true },
+	movement: Number,
+	spawnLocation: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' },
+	spawnStreet: { type: mongoose.Schema.Types.ObjectId, ref: 'Street' },
+	remnant: { type: Boolean, default: false, required: true },
+	health: { type: Number, required: true },
+	attackMod: { type: Number, required: true },
+	evadeMod: { type: Number, required: true },
+	healthDmg: { type: Number, required: true },
+	sanityDmg: { type: Number, required: true },
+	activation: { type: String, required: true } // hunter, patrol, or lurker
 });
 
 module.exports = mongoose.model('Monster', monsterSchema);
