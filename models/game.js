3b7823724fef69a303a560b5a71229ad9bc54da5
lcohen730 <lcohen730@gmail.com>
2025-03-04 00:09:22 -0500

    Add store and activeEvent properties to game model

--- models/game.js
+++ models/game.js
@@ -3,10 +3,12 @@ const Schema = mongoose.Schema;
 const commonMethods = require('./commonMethods');
 
 const gameSchema = new Schema({
-    investigators: [{ type: Schema.Types.ObjectId, ref: 'PlayerInvestigator' }],
-    scenario: { type: Schema.Types.ObjectId, ref: 'GameScenario' },
-    archive: { type: Array, required: true },
-    headlines: { type: Array, required: true }
+	investigators: [{ type: Schema.Types.ObjectId, ref: 'PlayerInvestigator' }],
+	scenario: { type: Schema.Types.ObjectId, ref: 'GameScenario' },
+	archive: { type: Array, required: true },
+	headlines: { type: Array, required: true },
+	store: [{ type: Schema.Types.ObjectId, ref: 'Item' }],
+	activeEvent: { type: Schema.Types.ObjectId, ref: 'Event' }
 });
 
 gameSchema.methods.addToArray = commonMethods.addToArray;
