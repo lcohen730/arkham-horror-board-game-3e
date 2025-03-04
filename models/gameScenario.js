cdd94f468b5ca46e3115ba4b956b06bf8602960d
lcohen730 <lcohen730@gmail.com>
2025-03-04 00:11:36 -0500

    Add eventDeck and eventDiscard properties to gameScenario model

--- models/gameScenario.js
+++ models/gameScenario.js
@@ -3,22 +3,24 @@ const Schema = mongoose.Schema;
 const commonMethods = require('./commonMethods');
 
 const gameScenarioSchema = new Schema({
-    mythosCup: { type: Array, required: true },
-    doom: { type: Number, required: true },
-    clues: { type: Number, required: true },
-    scenario: { type: Schema.Types.ObjectId, ref: 'Scenario' }
+	mythosCup: { type: Array, required: true },
+	doom: { type: Number, required: true },
+	clues: { type: Number, required: true },
+	eventDeck: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+	eventDiscard: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+	scenario: { type: Schema.Types.ObjectId, ref: 'Scenario' }
 });
 
 gameScenarioSchema.methods.removeFromMythosCup = function (tokenName) {
-    const index = this.mythosCup.indexOf(tokenName);
-    this.mythosCup.splice(index, 1);
-    return this.save();
-}
+	const index = this.mythosCup.indexOf(tokenName);
+	this.mythosCup.splice(index, 1);
+	return this.save();
+};
 
 gameScenarioSchema.methods.addToMythosCup = function (tokenName) {
-    this.mythosCup.push(tokenName);
-    return this.save();
-}
+	this.mythosCup.push(tokenName);
+	return this.save();
+};
 
 gameScenarioSchema.methods.updateAmt = commonMethods.updateAmt;
 
