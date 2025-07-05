3a7dd9594501db4cc696891023ef637109ff2e21
lcohen730 <lcohen730@gmail.com>
2025-07-05 16:07:15 -0400

    Pass playerinvestigators props to game page

--- src/pages/GamePage/GamePage.js
+++ src/pages/GamePage/GamePage.js
@@ -1,11 +1,11 @@
 import styles from './GamePage.module.scss';
 import GameArea from '../../components/GameArea/GameArea';
 
-export default function GamePage({ activeScenario }) {
+export default function GamePage({ activeScenario, playerInvestigators, setPlayerInvestigators }) {
     return (
         <div className={styles.gamePage}>
             <h2>Scenario: {activeScenario.name}</h2>
-            <GameArea />
+            <GameArea playerInvestigators={playerInvestigators} setPlayerInvestigators={setPlayerInvestigators} />
         </div>
     );
 }
